f666ceffdca84da927ed59129a7e04c6c2fb484d
Brendan Lesniak <brendan.lesniak@gmail.com>
2016-03-31 10:04:26 -0500

    Updated Statics/Templates root to options

--- serial-server/serial-server.py
+++ serial-server/serial-server.py
@@ -8,21 +8,22 @@ import time
 import serial
 import os
 
-define("port", default=8000, help="run on the given port", type=int)
-define("statics", default="statics/")
+define("server-port", default = 8000, help = "Starts the server on the given port", type = int)
+define("statics-root", default = "statics/")
+define("templates-root", default = "templates/")
 
 class SerialMonitorApplication(tornado.web.Application):
 	def __init__(self):
 		handlers=[
 			(r"/", IndexController),
-			(r"/statics/(.*)", tornado.web.StaticFileHandler, {'path': options.statics}),
+			(r"/statics/(.*)", tornado.web.StaticFileHandler, {'path': options.statics-root}),
 			(r"/serial/data-monitor", SerialDataSocket)
 		]
 		
 		settings = dict(
-			blog_title = u"Serial Monitor Application",
-			template_path = os.path.join(os.path.dirname(__file__), "templates"),
-			statics_path = os.path.join(os.path.dirname(__file__), "statics"),
+			app_title = u"Serial Monitor Application",
+			template_path = os.path.join(os.path.dirname(__file__), options.templates-root[:1]),
+			statics_path = os.path.join(os.path.dirname(__file__), options.statics-root[:1]),
 		)
 		
 		super(SerialMonitorApplication, self).__init__(handlers, **settings)
@@ -31,27 +32,27 @@ class IndexController(tornado.web.RequestHandler):
 	def get(self):
 		self.render('index.html')
  
-	class SerialDataSocket(tornado.websocket.WebSocketHandler):
-		clients = []
-    
-		def open(self):
-			print 'New connection - %s' % self
-			self.clients.append(self)
-			self.write_message('Connected to SerialData Socket')
+class SerialDataSocket(tornado.websocket.WebSocketHandler):
+	clients = []
 
-		def on_message(self, msg):
-			print 'Received: %s' % msg
-			self.write_message('Received: %s' % msg)
+	def open(self):
+		print 'New connection - %s' % self
+		self.clients.append(self)
+		self.write_message('Connected to SerialData Socket')
 
-		def on_close(self):
-			print 'Closed - %s' % self
-			self.clients.remove(self)
+	def on_message(self, msg):
+		print 'Received: %s' % msg
+		self.write_message('Received: %s' % msg)
+
+	def on_close(self):
+		print 'Closed - %s' % self
+		self.clients.remove(self)
 
 		
 def main():
 	tornado.options.parse_command_line()
 	httpServer = tornado.httpserver.HTTPServer(SerialMonitorApplication())
-	httpServer.listen(options.port)
+	httpServer.listen(options.server-port)
 
 	# Print so we know the server started
 	print "Listening on port:", options.port
