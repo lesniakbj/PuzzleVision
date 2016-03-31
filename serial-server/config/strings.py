ddf8771db0bd790b94c320fe8ff851282193f05c
Brendan Lesniak <brendan.lesniak@gmail.com>
2016-03-31 14:34:54 -0500

    Added Strings.py

---
+++ serial-server/config/strings.py
@@ -0,0 +1,5 @@
+class Strings():
+    HELP_SERVER_PORT = r'Start the server on the given port'
+    HELP_SERIAL_PORT = r'Start a UART connection on the given port'
+    HELP_STATICS = r'Where the server will look for static files'
+    HELP_TEMPLATES = r'Where the server will look for view templates'
